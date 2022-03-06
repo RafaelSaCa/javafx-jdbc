4ddc708e00dab03366319da2f9fa7c568ca05d64
RafaelSaCa <raffaellsaca@gmail.com>
2022-03-06 14:56:35 -0300

    Adicionado aceeso ao banco de dados

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -3,17 +3,28 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao(); //INJECAO DE DEPENDENCIA
+	
 	public List<Department> findAll(){
-		//mocar os dados - sem acesso ao banco
+		
+		return dao.findAll();
+		
+		
+		/*/mocar os dados - sem acesso ao banco
 		List<Department> list = new ArrayList<>();
 		list.add(new Department(1,"Books"));
 		list.add(new Department(2, "Computers"));
 		list.add(new Department(3, "Electronics"));
-		return list;
+		return list;*/
+		
+	
+		
 	}
 
 }
