cf76191cee31a6d9793ba9b40c261d0561041cb2
RafaelSaCa <raffaellsaca@gmail.com>
2022-03-10 21:01:26 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,151 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable{
+
+	private Seller entity;
+	private SellerService service;
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	
+		
+	
+	@FXML
+	private TextField txtId;
+	
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {//dependencia
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	
+	//salvar objeto
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade esta vazia");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service esta nulo");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();//fechar a janela ao salvar os dados
+		
+		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Erro ao salvar o objeto", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		//VALIDAÇÃO
+		ValidationException exception = new ValidationException("Erro de Validação");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if ( txtName.getText() == null || txtName.getText().trim().equals("")){
+			exception.addError("name", "Campo nao pode ser vazio!");
+		}
+		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size() > 0 ) {
+			throw exception;
+		}
+		 return obj;		
+	}
+	
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+	
+	
+		
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {//
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName,30);
+	}
+	
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entidade está vazia");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	//mensagem de validacao
+	private void setErrorMessages (Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")){
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+	
+	
+	
+}
